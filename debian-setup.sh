a0a8babe54db8089e598d74266d5ec6850682a8b
Flasher∅ <fadhilefend@gmail.com>
2025-08-07 21:43:23 +0700

    Add files via upload

---
+++ debian-setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# ------------------------------------------------------------------
+# This script automates the setup of a Debian environment for Termux.
+# ------------------------------------------------------------------
+
+echo ">>> Updating and upgrading Debian packages..."
+echo "This may take a while. Please ensure you have a stable internet connection."
+apt update && apt upgrade -y
+
+echo ">>> Installing necessary packages: sudo, nano, adduser, pulseaudio..."
+apt install -y sudo nano adduser pulseaudio
+
+echo ">>> Setting up a new user..."
+echo "You will be prompted to create a new user named 'user'."
+echo "Please enter a password when asked and remember it."
+echo "You can leave the other fields (Full Name, etc.) blank by pressing Enter."
+adduser user
+
+echo ">>> Granting root access to the new user..."
+# Create a new sudoers file for the user, which is safer than editing /etc/sudoers directly.
+echo 'user ALL=(ALL:ALL) ALL' >/etc/sudoers.d/user
+chmod 0440 /etc/sudoers.d/user
+echo "User 'user' has been granted sudo privileges."
+
+echo ">>> Installing XFCE4 Desktop Environment and Terminal..."
+echo "This will also take some time and requires a stable internet connection."
+apt install -y xfce4 xfce4-terminal
+
+echo ">>> Installing optional packages..."
+
+# Install Firefox web browser
+echo "Installing Firefox..."
+apt install -y firefox-esr
+
+# Install Onboard (on-screen keyboard)
+echo "Installing Onboard..."
+apt install -y onboard
+
+# Install Geany (IDE)
+echo "Installing Geany..."
+apt install -y geany
+
+echo "-----------------------------------------------------------------------------------------"
+echo ">>> Debian setup is complete!. you can run xfce4 from script in termux home directory"
+echo "You can now exit the Debian shell by typing 'exit'."
+echo "-----------------------------------------------------------------------------------------"
---
+++ termux-setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+echo "Updating and upgrading packages..."
+pkg update && pkg upgrade -y
+
+echo "Installing X11 repository..."
+pkg install x11-repo -y
+
+echo "Installing Termux X11, PulseAudio, Proot-Distro, Git, and Wget..."
+pkg install termux-x11-nightly pulseaudio proot-distro git wget -y
+
+echo "Cloning the repo to get the xfce4 debian script"
+git clone https://github.com/flasherxgapple/debian-termux.git
+cd debian-termux
+chmod +x debian.sh
+mv debian.sh ~/
+cd
+
+echo "------------------------------------------------------------------"
+echo "WARNING: There is a second script that must be run inside the"
+echo "Debian shell after this installation completes."
+echo "------------------------------------------------------------------"
+read -p "Press Enter to continue..."
+
+echo "Installing Debian..."
+proot-distro install debian
+
+echo "Installation complete."
